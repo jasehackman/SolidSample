49059371e84c9548986b40037f14539f75c7dc96
Jase Hackman <Jase.Hackman@healthstream.com>
2020-08-28 14:00:54 -0500

    OCP refcator complete

---
+++ ArdalisRating/Raters/UnknownPolicyRater.cs
@@ -0,0 +1,17 @@
+﻿using ArdalisRating.Raters;
+
+namespace ArdalisRating
+{
+    public class UnknownPolicyRater : Rater
+    {
+        public UnknownPolicyRater(RatingEngine engine, ConsoleLogger logger)
+            : base(engine, logger)
+        {
+        }
+
+        public override void Rate(Policy policy)
+        {
+            _logger.Log("Unknown policy type");
+        }
+    }
+}
